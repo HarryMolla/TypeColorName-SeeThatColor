2acbd10db5334f519dc4dc73f42f94b82a3bc3ca
unknown <hmiol5577@gmail.com>
2025-07-26 16:00:44 +0300

    with toogle

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,7 @@
-import React from 'react'
-
-const Input = ({colorValue, setColorValue}) => {
+import colorNames from 'colornames';
+const Input = ({
+  colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText
+}) => {
   return (
     <form onSubmit={(e)=>e.preventDefault()}>
        <label>Add Color Name:</label>
@@ -10,8 +11,18 @@ const Input = ({colorValue, setColorValue}) => {
            placeholder='Add color name'
            required
            value={colorValue}
-           onChange={(e)=> setColorValue(e.target.value)}
+           onChange={(e)=> { 
+              setColorValue(e.target.value);
+              setHexValue(colorNames(e.target.value));
+
+            }}
            />
+           <button
+             type='button'
+             onClick={()=> setIsDarkText(!isDarkText)}
+           >
+            Toogle Text Color
+           </button>
     </form>
   )
 }
--- src/Square.js
+++ src/Square.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const Square = ({colorValue}) => {
+const Square = ({colorValue, hexValue, isDarkText}) => {
   return (
     <section
       className="square"
-      style={{backgroundColor: colorValue}}
+      style={{
+        backgroundColor: colorValue,
+        color: isDarkText ? "#000" : "#FFF",
+       // border: "1px solid #333",
+        //fontSize: "2rem"
+      }}
       >
       <p>{colorValue ? colorValue:"Empty Value"}</p>
+      <p>{hexValue ? hexValue:"No hex value"}</p>
     </section>
   )
 }
